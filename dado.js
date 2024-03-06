4b190be89c5925e09fa450d0aa1296da41e2b9cc
Marcelly Dias <111815492+marcellydias@users.noreply.github.com>
2024-03-06 16:41:32 -0300

    Create dado.js

---
+++ dado.js
@@ -0,0 +1,52 @@
+input.onGesture(Gesture.Shake, function () {
+    if (randint(0, 6) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (randint(0, 6) == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (randint(0, 6) == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (randint(0, 6) == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (randint(0, 6) == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+
